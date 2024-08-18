c98da3622157f11f56f8a30757e7b30536fe311d
Justin Simon <justinsimon@Justins-MBP.lan>
2024-08-18 14:40:49 -0700

    Created config settings

---
+++ ORM-Dapper/Department.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ORM_Dapper
+{
+	public class Department
+	{
+		public Department()
+		{
+		}
+	}
+}
+
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace ORM_Dapper
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
         }
     }
 }
