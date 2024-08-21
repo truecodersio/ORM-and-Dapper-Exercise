4d177df3625fce85aad899b6d77a1011bc08bc64
Justin Simon <justinsimon@Justins-MBP.lan>
2024-08-21 03:25:52 -0700

    Created Product dapper methods, not fully scoped

--- ORM-Dapper/DapperProductRepository.cs
+++ ORM-Dapper/DapperProductRepository.cs
@@ -1,11 +1,37 @@
 ﻿using System;
+using System.Data;
+using Dapper;
 namespace ORM_Dapper
 {
-	public class DapperProductRepository
+	public class DapperProductRepository : IProductRepository
 	{
-		public DapperProductRepository()
+		//Private readonly conneciton so that it cant be changed after being initialized
+		 private readonly IDbConnection _conn;
+
+		public DapperProductRepository(IDbConnection connection)
 		{
+			connection = _conn;
 		}
-	}
+
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+			//_conn.Execute("INSERT INTO departments Name Values(@name);", new { name = name });
+             _conn.Execute("INSERT INTO products Name Values(@name);" , new {name = name});
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+		  return _conn.Query<Product>("SELECT * FROM products");
+        }
+		public IEnumerable<Product> UpdateProduct()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Product> DeleteProduct()
+		{
+			throw new NotImplementedException();
+		}
+    }
 }
 
--- ORM-Dapper/Product.cs
+++ ORM-Dapper/Product.cs
@@ -3,9 +3,13 @@ namespace ORM_Dapper
 {
 	public class Product
 	{
-		public Product()
-		{
-		}
+		public int productId { get; set; }
+		public string name { get; set; }
+		public int price { get; set; }
+		public int categoryID { get; set; }
+		public bool onSale { get; set; }
+		public string stockLevel { get; set; }
+		
 	}
 }
 
